161b089b8550bd3be3f0e35e3659b219eebcfa95
Bryan de Oliveira <brdebr.93@gmail.com>
2019-01-28 16:21:08 +0100

    Add page transition

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,7 +32,9 @@ module.exports = {
   /*
   ** Global CSS
   */
-  css: ['~/assets/style/app.styl'],
+  css: [
+    '~/assets/style/custom-transitions.css',
+    '~/assets/style/app.styl'],
 
   /*
   ** Plugins to load before mounting the App
