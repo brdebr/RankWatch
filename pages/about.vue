3c6a5a23ac12fcf6297edcf71eccbbaab95dfc64
Bryan de Oliveira <brdebr.93@gmail.com>
2019-01-28 16:22:12 +0100

    Add placeholder content to pages

--- pages/about.vue
+++ pages/about.vue
@@ -1,19 +1,32 @@
 <template>
-  <v-layout>
-    <v-flex text-xs-center>
-      <img
-        src="/v.png"
-        alt="Vuetify.js"
-        class="mb-5"
-      >
-      <blockquote class="blockquote">
-        &#8220;First, solve the problem. Then, write the code.&#8221;
-        <footer>
-          <small>
-            <em>&mdash;John Johnson</em>
-          </small>
-        </footer>
-      </blockquote>
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+    <v-flex
+      xs12
+      sm8
+      md6
+      class="mb-auto"
+    >
+      <v-card>
+        <v-card-title class="headline" id="ddss">
+          This is the About Page
+        </v-card-title>
+        <v-card-text>
+          <p>This is the about page</p>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora, facere. Minima nostrum libero reiciendis culpa eaque cum iure totam officiis, iste corporis, voluptates animi laboriosam fugit repellat explicabo debitis. Nihil!</p>
+        </v-card-text>
+      </v-card>
     </v-flex>
   </v-layout>
 </template>
+
+<script>
+
+export default {
+  components: {
+  }
+}
+</script>
