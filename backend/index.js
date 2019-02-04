aa7d31b652cdb886291b4b6fb6a9051c8819f6b1
Bryan de Oliveira <brdebr.93@gmail.com>
2019-02-04 16:28:34 +0100

    Add HelloWorld API

---
+++ backend/index.js
@@ -0,0 +1,55 @@
+// Modules
+const express = require('express');
+const bodyParser = require('body-parser')
+const morgan = require('morgan');
+
+// Express
+const app = express();
+const port = process.env.PORT || 4000;
+const www = process.env.WWW || './';
+
+// Settings
+app.set(port, (req, res) => {
+    console.log(`Request to port ${port}`);
+});
+
+// Middlewares
+
+// Morgan -> Sends messages like 
+// "GET / 404 3.307 ms - 139"
+app.use(morgan('dev'));
+
+// Allow express accept JSON's
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+
+// Allow CORS
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+    )
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    next()
+  })
+
+
+// Routes
+app.use('/',(req, res) =>{
+    console.log("Request to route: \"/\"");
+    res.json({
+        message:'Hello World',
+        status:'OK :D',
+    });
+});
+
+// - Server up
+app.listen(port, () => {
+    console.log(
+        `\n🚀 Express server API up! :D\n` +
+          `  - Start time: ${new Date(Date.now()).toLocaleString()}\n` +
+          `  - Port: ${port}\n`
+      )
+});
