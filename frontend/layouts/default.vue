643c88e6eac6975344fca8bec3ae2be74ebdc716
Bryan de Oliveira Brettas <brdebr.93@gmail.com>
2019-08-16 21:16:28 +0200

    Add background and particles

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -2,8 +2,14 @@
   <v-app dark>
     <TheDrawer />
     <TheToolbar app-name="RankWatch" />
-    <v-content>
-      <v-container>
+    <v-content class="bg-gradient">
+      <no-ssr>
+        <vue-particles
+          class="bg-particles"
+          particles-json="/particles-conf.json"
+        />
+      </no-ssr>
+      <v-container class="pb-5" fluid>
         <nuxt />
       </v-container>
     </v-content>
@@ -24,3 +30,51 @@ export default {
   }
 }
 </script>
+<style lang="scss">
+.bg-particles {
+  position: fixed;
+  width: 100%;
+  height: 100%;
+}
+.bg-gradient {
+  background: rgba(242, 246, 248, 1);
+  background: -moz-linear-gradient(
+    top,
+    rgba(242, 246, 248, 1) 0%,
+    rgba(216, 225, 231, 1) 55%,
+    rgba(181, 198, 208, 1) 100%
+  );
+  background: -webkit-gradient(
+    left top,
+    left bottom,
+    color-stop(0%, rgba(242, 246, 248, 1)),
+    color-stop(55%, rgba(216, 225, 231, 1)),
+    color-stop(100%, rgba(181, 198, 208, 1))
+  );
+  background: -webkit-linear-gradient(
+    top,
+    rgba(242, 246, 248, 1) 0%,
+    rgba(216, 225, 231, 1) 55%,
+    rgba(181, 198, 208, 1) 100%
+  );
+  background: -o-linear-gradient(
+    top,
+    rgba(242, 246, 248, 1) 0%,
+    rgba(216, 225, 231, 1) 55%,
+    rgba(181, 198, 208, 1) 100%
+  );
+  background: -ms-linear-gradient(
+    top,
+    rgba(242, 246, 248, 1) 0%,
+    rgba(216, 225, 231, 1) 55%,
+    rgba(181, 198, 208, 1) 100%
+  );
+  background: linear-gradient(
+    to bottom,
+    rgba(242, 246, 248, 1) 0%,
+    rgba(216, 225, 231, 1) 55%,
+    rgba(181, 198, 208, 1) 100%
+  );
+  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#f2f6f8', endColorstr='#b5c6d0', GradientType=0 );
+}
+</style>
--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -30,7 +30,7 @@ export default {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: [],
+  plugins: ['@/plugins/particles.client'],
   /*
    ** Nuxt.js dev-modules
    */
